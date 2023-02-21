55aa2cdcc552c8002da0792e6f1b817981f74521
AbdullahQamarSe <113988815+AbdullahQamarSe@users.noreply.github.com>
2023-02-20 21:34:12 -0800

    Update sel.py

--- sel.py
+++ sel.py
@@ -1,24 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import time
+"""
 from nordvpn_connect import initialize_vpn, rotate_VPN, close_vpn_connection
 
-# Set up the Chrome options
-chrome_options = Options()
+# optional, use this on Linux and if you are not logged in when using nordvpn command
 
+# optional, use this on Linux and if you are not logged in when using nordvpn command
 
-settings = initialize_vpn("France")  # starts nordvpn and stuff
+settings = initialize_vpn("France", "1hZV8hPzFtW2YGhZ6pZPY7gK", "ZaBq4vm9cfZVYrGbKigAjTZ4")  # starts nordvpn and stuff
 rotate_VPN(settings)  # actually connect to server
+print('hello')
 
-# YOUR STUFF
+close_vpn_connection(settings)
 
+"""
+from nordvpn_switcher import initialize_VPN,rotate_VPN
+import time
 
-# Set up the VPN
-# # Replace with your VPN server address and port
+instr = initialize_VPN(area_input=["spain"],skip_settings=1)
 
-# Start the Chrome driver with the configured options
-driver = webdriver.Chrome(options=chrome_options)
-
-# Navigate to a website to check your location
-driver.get('https://www.whatismyip.com/')
-time.sleep(100)
+for i in range(3):
+    rotate_VPN(instr)
+    print('\nDo whatever you want here (e.g. scraping). Pausing for 10 seconds...\n')
+    time.sleep(10)
